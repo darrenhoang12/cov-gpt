55919c7e0c2531aa6e74de0666ea7b43d87df04c
Darren Hoang <darrenhoang12@gmail.com>
2023-11-05 12:46:36 -0800

    Added technology stack

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -3,8 +3,17 @@ import { Element } from "react-scroll";
 
 function About() {
   return (
-    <Element name="about" className="about">
-      <p>HEllo I am about</p>
+    <Element name="about">
+      <h2 className="centered-h1">Built with technologies of the future!</h2>
+      <div className="about">
+        <div className="tech-stack">
+          <div className="tech">React</div>
+          <div className="tech">Node</div>
+          <div className="tech">ChatGPT</div>
+          <div className="tech">Cheerio</div>
+          <div className="tech">GitHub</div>
+        </div>
+      </div>
     </Element>
   );
 }
